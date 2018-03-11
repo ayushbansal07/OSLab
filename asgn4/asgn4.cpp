712e7a620d99728fc23cdd3c2e1376ae4ba1c48e
Ayush <abansal1008@gmail.com>
2018-03-11 20:00:29 +0000

    error fixes

--- asgn4/asgn4.cpp
+++ asgn4/asgn4.cpp
@@ -27,32 +27,34 @@ int create_myfs(int size)
 	if(size > MAX_ALLOCATED_SIZE) return -1;
 	if(size*MBtoB < SUPERBLOCK_SIZE + INODE_LIST_BYTES) return -1;
 	myfs = new char[size*MBtoB];
+	//Initialise Super Block
 	SuperBlock* sb = (SuperBlock *) myfs;
-
 	sb->total_sz = size * MBtoB;
 	sb->max_inodes = 	MAX_INODES;
 	sb->actual_inodes = 1; //root by default
 	sb->max_diskBlocks = MAX_NO_OF_DISKBLOCKS;
 	sb->actual_diskBlocks = SUPERBLOCK_BLOCKS + INODE_LIST_BLOCKS + 1;
-	for(int i=0;i<INODE_LIST_BYTES;i++)
+	for(int i=0;i<MAX_INODES;i++)
 		sb->free_inodes[i] = 0;
 	sb->free_inodes[0] = 1;
-	int i = 0;
-	for(i=0;i<( SUPERBLOCK_BLOCKS + INODE_LIST_BLOCKS + 1)/8;i++)
+	int i;
+	for(i=0;i<SUPERBLOCK_BLOCKS + INODE_LIST_BLOCKS + 1;i++)
 		sb->free_diskBlocks[i] = 1;
-	
+	//Initialise Inode List
 	InodeList* inl = (InodeList *)(myfs + SUPERBLOCK_BYTES);
 	inl->node[0].filetype = 1;
 	inl->node[0].filesize = 0;
 	inl->node[0].last_modified = 	time(NULL);
 	inl->node[0].last_read = time(NULL);
-	strcpy(inl->node[0].access_permission , "666");
+	inl->node[0].access_permission[0] = 6;
+	inl->node[0].access_permission[1] = 6;
+	inl->node[0].access_permission[2] = 6;
 	inl->node[0].direct_pointers[0] = sb->actual_diskBlocks -1;
 	for(int i=1;i<8;i++)
 		inl->node[0].direct_pointers[i] = -1;
 	inl->node[0].indirect_pointer = -1;
 	inl->node[0].doubly_indirect_pointer = -1;
-
+	//Initialise root directory
 	Directory* root_block = (Directory *)(myfs +SUPERBLOCK_BYTES + INODE_LIST_BYTES);
 	init_dir(root_block);
 
@@ -89,7 +91,7 @@ int copy_pc2myfs(char* source, char* dest)
 	int filesize = file.tellg();
 	file.seekg(0,ios::beg);
 	int no_of_blocks = filesize/DISKBLOCK_SIZE + 1;
-	int available_blocks = sb->actual_inodes - sb->actual_diskBlocks;
+	int available_blocks = sb->max_diskBlocks - sb->actual_diskBlocks;
 	if(no_of_blocks > available_blocks)
 	{
 		cout<<"Disk Space not SUfficient"<<endl;
@@ -98,18 +100,18 @@ int copy_pc2myfs(char* source, char* dest)
 	int inode_idx = get_inode();
 	if(inode_idx == -1)
 	{
-		cout<<"Error creeating file: Unable to get new inode"<<endl;
+		cout<<"Error creating file: Unable to get new inode"<<endl;
 		return -1;
 	}
 	InodeList * inl = (InodeList *)(myfs + SUPERBLOCK_BLOCKS);
-	inl->node->filetype = 0;
-	inl->node->filesize = filesize;
-	inl->node->last_modified = time(NULL);
-	inl->node->last_read = time(NULL);
-	inl->node->access_permission[0] = 6;
-	inl->node->access_permission[1] = 6;
-	inl->node->access_permission[2] = 4;
-
+	inl->node[inode_idx].filetype = 0;
+	inl->node[inode_idx].filesize = filesize;
+	inl->node[inode_idx].last_modified = time(NULL);
+	inl->node[inode_idx].last_read = time(NULL);
+	inl->node[inode_idx].access_permission[0] = 6;
+	inl->node[inode_idx].access_permission[1] = 6;
+	inl->node[inode_idx].access_permission[2] = 4;
+	
 
 
 
